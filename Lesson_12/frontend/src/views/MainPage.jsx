a0b4994c8e470e5497baf40dc7650dc83bf89b52
Alex <alexzbu@gmail.com>
2025-01-03 18:23:27 +0100

    bug fixed

--- Lesson_12/frontend/src/views/MainPage.jsx
+++ Lesson_12/frontend/src/views/MainPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const MainPage = () => {
    return (
@@ -6,7 +7,7 @@ const MainPage = () => {
          <section className="intro">
             <h1 className="intro__title">Welcome to the fleet!</h1>
             <p className="intro__text">We offer the best vehicles for your comfort and safety.</p>
-            <a href="/cars" className="intro__button">View the fleet</a>
+            <Link to="/cars" className="intro__button">View the fleet</Link>
          </section>
       </div>
    )
