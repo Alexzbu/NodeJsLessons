e0b0dcb2edb686e844eadfc5a33e5806b6d1aa71
Alex <alexzbu@gmail.com>
2025-03-21 17:35:55 +0100

    fixed url

--- Lesson_13/frontend/src/api/indexApi.js
+++ Lesson_13/frontend/src/api/indexApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiServer = axios.create({
-   baseURL: 'http://localhost:8080/api/',
+   baseURL: 'https://euphoria-back.onrender.com/api/',
    headers: {
       'Content-Type': 'application/json',
       // 'Authorization': `Bearer ${localStorage.getItem('jwt_token')}`
